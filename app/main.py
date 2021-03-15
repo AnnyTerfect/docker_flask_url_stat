5f49ee1faa4a8dab67aef34af17a2114af0a973e
AnnyTerfect <1247470246@qq.com>
2021-03-15 21:28:18 +0800

    first commit

---
+++ app/main.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+from os.path import exists
+app = Flask(__name__)
+
+@app.route('/get', methods=['GET'])
+def get():
+	hname = str(hash(request.environ['QUERY_STRING']))
+	print(hname)
+	if not(exists(hname)):
+		with open(hname, 'w') as wf:
+			wf.write('0')
+	with open(hname) as rf:
+		return rf.read().strip()
+	return 'error'
+
+@app.route('/add', methods=['GET'])
+def add():
+	hname = str(hash(request.environ['QUERY_STRING']))
+	print(hname)
+	if not(exists(hname)):
+		with open(hname, 'w') as wf:
+			wf.write('0')
+	with open(hname) as rf:
+		num = int(rf.read().strip())
+		with open(hname, 'w') as wf:
+			wf.write(str(num + 1))
+			return 'successful'
+	return 'error'
+
+if __name__ == "__main__":
+    # Only for debugging while developing
+    app.run(host='0.0.0.0', debug=True, port=80)
